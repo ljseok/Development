17596e53114c343419911a4cb653e1a5e6099ec0
ccccljs <cho38suk@hamail.net>
2021-10-26 14:15:22 +0900

    상하좌우 이동

--- 상하좌우.py
+++ 상하좌우.py
@@ -1,18 +1,19 @@
-n = 5
+n = int(input()) # N을 입력받기
 x,y = 1,1
-plans=['R','R','R','U','D','D']
+plans = input().split()
 
-dx=[0,0,-1,1] # L= 0,-1 R= 0,1 U= -1,0 D= 1,0
-dy=[-1,1,0,0]
-
-move=['L','R','U','D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+# L,R,U,D에 따른 이동 방향
+move = ['L','R','U','D']
 
 for plan in plans:
     for i in range(len(move)):
         if plan == move[i]:
-            nx = x + dx[i] # 어느 방향으로 갈것인지
+            nx = x + dx[i]
             ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n: # 공간을 벗어나는 경우 무시한다.
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
-    x,y = nx,ny
-print(x, y)
+    x, y = nx, ny
+print(x,y)
+
